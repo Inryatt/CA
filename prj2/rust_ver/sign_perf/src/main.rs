b7f893424f2a40ee7a978f131ed58eddc388421d
Inryatt <inrymail@gmail.com>
2022-12-20 12:08:56 +0000

    RSA timings done

--- prj2/rust_ver/sign_perf/src/main.rs
+++ prj2/rust_ver/sign_perf/src/main.rs
@@ -2,35 +2,65 @@ use std::vec;
 
 use rsa::{RsaPublicKey, RsaPrivateKey, pkcs1::DecodeRsaPublicKey, pkcs1::DecodeRsaPrivateKey   };
 use rsa::pkcs1v15::{SigningKey, VerifyingKey};
-use rsa::signature::{RandomizedSigner, Signature, Verifier};
+use rsa::pss::{BlindedSigningKey, VerifyingKey as PSSVerifyingKey};
+use rsa::signature::{RandomizedSigner, Signature, Verifier, Keypair};
 use sha2::{Digest, Sha256};
+use rsa::{ pkcs8::DecodePublicKey};
+
 
 // list with key sizes
-fn rsa_load_keys()-> Vec<(RsaPublicKey, RsaPrivateKey)> {
+fn rsa_load_keys()-> Vec<(RsaPrivateKey)> {
     let key_size = vec![1024, 2048, 4096];
     let mut keys = vec![];
 
     for size in key_size {
-        let pub_filename = format!("../../keys/{}_public_key.pem", size);
-        let pubkey = std::fs::read_to_string(pub_filename).unwrap();
-        let public_key = RsaPublicKey::from_pkcs1_pem(&pubkey).expect("Failed to read public key!");
+       let priv_filename = format!("../../keys/{}_private_key.pem", size);
+        let private_key = RsaPrivateKey::read_pkcs1_pem_file(&priv_filename).expect("Failed to read private key!");
         
-        let priv_filename = format!("../../keys/{}_private_key.pem", size);
-        let privkey = std::fs::read_to_string(priv_filename).unwrap();
-        let private_key = RsaPrivateKey::from_pkcs1_pem(&privkey).expect("Failed to read private key!");
-        
-        let keypair = (public_key, private_key);
-        keys.push(keypair);
+        keys.push(private_key);
     }
     return keys;
 }
 
+
 fn rsa_perf(){
-    let KEYS = rsa_load_keys();
-    for keypair in KEYS{
-        let (public_key, private_key) = keypair;
-        let signing_key = SigningKey::<Sha256>::new_with_prefix(private_key);
-        let verifying_key: VerifyingKey<_> = (&signing_key).into();
+
+    // PKCS1
+    let mut rng = rand::thread_rng();
+    let keys = rsa_load_keys();
+    for keypair in keys{
+        let (private_key) = keypair;      
+        let signing_key = SigningKey::<Sha256>::new(private_key);
+        let verifying_key: VerifyingKey<_> = (&signing_key).into();    
+
+
+        // get unix timestamp
+        let start = std::time::SystemTime::now();
+        let data = b"this is a message";
+        let signature = signing_key.sign_with_rng(&mut rng, data);
+        verifying_key.verify(data, &signature).expect("[!] - failed to verify!!");
+        let end = std::time::SystemTime::now();
+        let duration = end.duration_since(start).expect("Time went backwards");
+        println!("PKCS1: {} ms", duration.as_millis());
+
+    }
+    //PSS
+
+    let keys = rsa_load_keys();
+    for keypair in keys{
+        let ( private_key) = keypair;      
+        let signing_key = BlindedSigningKey::<Sha256>::new(private_key);
+        let verifying_key: PSSVerifyingKey<_> = (&signing_key).into();    
+
+
+        // get unix timestamp
+        let start = std::time::SystemTime::now();
+        let data = b"this is a message";
+        let signature = signing_key.sign_with_rng(&mut rng, data);
+        verifying_key.verify(data, &signature).expect("[!] - failed to verify!!");
+        let end = std::time::SystemTime::now();
+        let duration = end.duration_since(start).expect("Time went backwards");
+        println!("PSS: {} ms", duration.as_millis());
     }
         
 }
