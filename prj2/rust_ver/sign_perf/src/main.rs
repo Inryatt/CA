8a7e320c90d4142d6f3e3ccd24a98b65296590c4
Inryatt <inrymail@gmail.com>
2022-12-20 16:42:22 +0000

    ONE elliptic curve is working in rust

--- prj2/rust_ver/sign_perf/src/main.rs
+++ prj2/rust_ver/sign_perf/src/main.rs
@@ -1,5 +1,7 @@
-use std::vec;
+use std::net::SocketAddr;
+use std::{vec, fs};
 
+use p256::NistP256;
 use rsa::{RsaPublicKey, RsaPrivateKey, pkcs1::DecodeRsaPublicKey, pkcs1::DecodeRsaPrivateKey   };
 use rsa::pkcs1v15::{SigningKey, VerifyingKey};
 use rsa::pss::{BlindedSigningKey, VerifyingKey as PSSVerifyingKey};
@@ -7,6 +9,16 @@ use rsa::signature::{RandomizedSigner, Signature, Verifier, Keypair};
 use sha2::{Digest, Sha256};
 use rsa::{ pkcs8::DecodePublicKey};
 
+
+
+use p256::{
+    ecdsa::{
+        signature::{Signer, Verifier as ECVerifier},
+        SigningKey as ECSigningKey, VerifyingKey as ECVerifyingKey,
+    },
+    pkcs8::EncodePrivateKey,
+    PublicKey, SecretKey as p256SecretKey,
+};
 
 // list with key sizes
 fn rsa_load_keys()-> Vec<(RsaPrivateKey)> {
@@ -65,6 +77,50 @@ fn rsa_perf(){
         
 }
 
+fn ec_load_nistp256_key()-> p256SecretKey {
+
+        //nist p256
+        
+        let priv_filename = format!("../../keys/nistp_small_private_key.pem");
+
+        let privk = fs::read_to_string(priv_filename).expect("Failed to Read file");
+
+
+
+
+
+
+
+
+
+        let private_key = p256::SecretKey::from_sec1_pem(&privk).expect("Failed to read private key!");
+        
+    return private_key;
+}
+
+fn ec_perf() {
+  
+    let secret_key = ec_load_nistp256_key();
+
+    // Derive public key
+    let public_key = secret_key.public_key();
+
+
+    // Signing
+    let start = std::time::SystemTime::now();
+    let signing_key: ECSigningKey = secret_key.into();
+    let message = b"ECDSA proves knowledge of a secret number in the context of a single message";
+    let signature = signing_key.sign(message);
+
+    // Verification
+    let verifying_key: ECVerifyingKey = public_key.into();
+    let end = std::time::SystemTime::now();
+    let duration = end.duration_since(start).expect("Time went backwards");
+    println!("NIST P-256: {} ms", duration.as_millis());
+    assert!(verifying_key.verify(message, &signature).is_ok());
+}
+
 fn main() {
-    rsa_perf()
+    rsa_perf();
+    ec_perf();
 }
